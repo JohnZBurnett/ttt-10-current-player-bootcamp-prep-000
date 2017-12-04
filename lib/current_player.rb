885ccc4564be3477e163d8d81ae01c861dbec109
John Burnett <tuninator@gmail.com>
2017-12-04 12:22:18 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,26 @@
+def current_player(board)
+  which_turn = turn_count(board)
+
+  if which_turn % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+
+end 
+
+def turn_count(board)
+
+  counter = 0 
+
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter += 1
+    end 
+
+  return counter 
+  end
+  
+      
+    
+  
